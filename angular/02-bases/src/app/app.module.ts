d48e67aab14e215c62e31f43f4a049d42a4f5bf5
jhonpulido1990 <3825holbertonstudents.com>
2024-02-25 17:19:01 -0500

    modulos

--- angular/02-bases/src/app/app.module.ts
+++ angular/02-bases/src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { CounterModule } from './counter/components/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CounterModule, HeroesModule],
+  imports: [BrowserModule, AppRoutingModule, CounterModule, HeroesModule, DbzModule],
   providers: [],
   bootstrap: [AppComponent],
 })
