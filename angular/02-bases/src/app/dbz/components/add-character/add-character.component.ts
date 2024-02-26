45da914038171d866da0351fcb77a262e8aa6b26
jhonpulido1990 <3825holbertonstudents.com>
2024-02-26 09:43:09 -0500

    FormsModule y ngModel

--- angular/02-bases/src/app/dbz/components/add-character/add-character.component.ts
+++ angular/02-bases/src/app/dbz/components/add-character/add-character.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'app-add-character',
   templateUrl: './add-character.component.html',
   styleUrls: ['./add-character.component.css']
 })
-export class AddCharacterComponent implements OnInit {
+export class AddCharacterComponent {
 
-  constructor() { }
+  public character: Character = {
+    name: '',
+    power: 0
+  };
 
-  ngOnInit() {
+  emitCharacter(): void {
+    console.log(this.character)
   }
 
 }
