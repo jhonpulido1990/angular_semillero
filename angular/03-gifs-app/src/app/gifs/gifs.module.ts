dd860c27555103be97a1a7031571d8c9ff5856f9
jhonpulido1990 <3825holbertonstudents.com>
2024-02-27 17:11:32 -0500

    Crad list

--- angular/03-gifs-app/src/app/gifs/gifs.module.ts
+++ angular/03-gifs-app/src/app/gifs/gifs.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { HomeComponent } from './pages/home/home.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomeComponent, SearchBoxComponent, CardListComponent],
+  imports: [CommonModule],
+  exports: [HomeComponent],
 })
-export class GifsModule { }
+export class GifsModule {}
