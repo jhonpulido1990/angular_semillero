ac592f84c29a23b45015eba1069359a2f9a95be8
jhonpulido1990 <3825holbertonstudents.com>
2024-02-28 16:07:14 -0500

    peticion http

--- angular/03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ angular/03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,31 +1,43 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class GifsService {
-
   private _tagsHistory: string[] = [];
+  private apiKey: string = 'Zj7mKjgJ9ymMZvE6N6LUKbvSLXsQXeCu';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() { }
+  constructor( private http: HttpClient ) {}
 
   get tagsHistory() {
     return [...this._tagsHistory];
   }
 
-  private oganizeHistory( tag: string ) {
+  private oganizeHistory(tag: string) {
     tag = tag.toLowerCase();
 
-    if ( this._tagsHistory.includes(tag) ) {
-      this._tagsHistory = this._tagsHistory.filter( (oldTag) => oldTag !== tag )
+    if (this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag);
     }
 
-    this._tagsHistory.unshift(tag)
-    this._tagsHistory = this._tagsHistory.splice(0, 10)
+    this._tagsHistory.unshift(tag);
+    this._tagsHistory = this._tagsHistory.splice(0, 10);
   }
 
-  searchTag( tag: string): void {
-    if ( tag.length === 0) return;
+  searchTag(tag: string):void {
+    if (tag.length === 0) return;
 
     this.oganizeHistory(tag);
-  }
 
+    const params = new HttpParams()
+    .set('api_key', this.apiKey)
+    .set('limit', '10')
+    .set('q', tag)
+
+    this.http.get(`${ this.serviceUrl }/search`, { params })
+    .subscribe( resp => {
+      console.log(resp)
+    } )
+
+  }
 }
