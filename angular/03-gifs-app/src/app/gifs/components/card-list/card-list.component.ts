65c5ba53ded6daf82aa3cb5c1a99181186974649
jhonpulido1990 <3825holbertonstudents.com>
2024-02-28 17:15:19 -0500

    mostrar los gifs en pantalla

--- angular/03-gifs-app/src/app/gifs/components/card-list/card-list.component.ts
+++ angular/03-gifs-app/src/app/gifs/components/card-list/card-list.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Gif } from '../../interfaces/gifs.interfaces';
 
 @Component({
   selector: 'app-card-list',
   templateUrl: './card-list.component.html',
   styleUrls: ['./card-list.component.css']
 })
-export class CardListComponent implements OnInit {
+export class CardListComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  @Input()
+  public gifs: Gif[] = []
 
 }
--- angular/03-gifs-app/src/app/gifs/pages/home/home.component.ts
+++ angular/03-gifs-app/src/app/gifs/pages/home/home.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+import { Gif } from '../../interfaces/gifs.interfaces';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
+  constructor( private gifsService: GifsService ) { }
 
-  ngOnInit() {
+  get gifs(): Gif[] {
+    return this.gifsService.gifList;
   }
 
 }
