d82fd1e4edf804451cedc7b0cbd85458c746a2fd
jhonpulido1990 <3825holbertonstudents.com>
2024-02-29 17:43:11 -0500

    tarea

--- angular/04-countryApp/src/app/shared/shared.module.ts
+++ angular/04-countryApp/src/app/shared/shared.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { HomePageComponent } from './pages/homePage/homePage.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
-
-
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
-  declarations: [HomePageComponent, AboutPageComponent],
-  imports: [
-    CommonModule
-  ],
-  exports: [HomePageComponent, AboutPageComponent]
+  declarations: [HomePageComponent, AboutPageComponent, SidebarComponent, ContactComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [HomePageComponent, AboutPageComponent, SidebarComponent, ContactComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
