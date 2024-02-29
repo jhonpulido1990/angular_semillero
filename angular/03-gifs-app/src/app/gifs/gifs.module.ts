7199e716c08d3c714f5364614f02ce16d8a54057
jhonpulido1990 <3825holbertonstudents.com>
2024-02-29 14:17:40 -0500

    lazy image parte 1

--- angular/03-gifs-app/src/app/gifs/gifs.module.ts
+++ angular/03-gifs-app/src/app/gifs/gifs.module.ts
@@ -1,14 +1,17 @@
-import { GifsCardComponent } from './components/gifs-card/gifs-card.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
+
+import { CardListComponent } from './components/card-list/card-list.component';
+import { GifsCardComponent } from './components/gifs-card/gifs-card.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
-import { CardListComponent } from './components/card-list/card-list.component';
+
 
 @NgModule({
   declarations: [HomeComponent, SearchBoxComponent, CardListComponent, GifsCardComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   exports: [HomeComponent],
 })
 export class GifsModule {}
---
+++ angular/03-gifs-app/src/app/shared/components/lazy-Image/lazy-Image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lazy-Image',
+  templateUrl: './lazy-Image.component.html',
+  styleUrls: ['./lazy-Image.component.css']
+})
+export class LazyImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
