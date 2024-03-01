c26f2711cff8a430af4752e16fd7e06bad70b77e
jhonpulido1990 <3825holbertonstudents.com>
2024-03-01 15:32:42 -0500

    country service

--- angular/04-countryApp/src/app/app.module.ts
+++ angular/04-countryApp/src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
+    HttpClientModule,
     SharedModule
   ],
   providers: [],
