d9bc5158d69be0dd84e066d400347beaced88ae7
jhonpulido1990 <3825holbertonstudents.com>
2024-03-01 23:47:17 -0500

    mostrar paises

--- angular/04-countryApp/src/app/countries/services/countries.service.ts
+++ angular/04-countryApp/src/app/countries/services/countries.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
 
 @Injectable({providedIn: 'root'})
-export class ServiceNameService {
+export class CountriesService {
 
   private apiUrl: string ='https://restcountries.com/v3.1';
 
