e000cf6b6fe4b31fc9c48018c35634df1396b699
jhonpulido1990 <3825holbertonstudents.com>
2024-03-06 17:13:04 -0500

    loading component

---
+++ angular/04-countryApp/src/app/shared/components/loadingSpinner/loadingSpinner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loadingSpinner',
+  templateUrl: './loadingSpinner.component.html',
+  styleUrls: ['./loadingSpinner.component.css']
+})
+export class LoadingSpinnerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- angular/04-countryApp/src/app/shared/shared.module.ts
+++ angular/04-countryApp/src/app/shared/shared.module.ts
@@ -5,24 +5,27 @@ import { RouterModule } from '@angular/router';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { HomePageComponent } from './pages/homePage/homePage.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LoadingSpinnerComponent } from './components/loadingSpinner/loadingSpinner.component';
 import { SearchBoxComponent } from './components/searchBox/searchBox.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AboutPageComponent,
     ContactComponent,
     HomePageComponent,
+    LoadingSpinnerComponent,
+    SearchBoxComponent,
     SidebarComponent,
-    SearchBoxComponent
   ],
   imports: [CommonModule, RouterModule],
   exports: [
     AboutPageComponent,
     ContactComponent,
     HomePageComponent,
+    LoadingSpinnerComponent,
+    SearchBoxComponent,
     SidebarComponent,
-    SearchBoxComponent
   ],
 })
 export class SharedModule {}
