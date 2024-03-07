8cd50a83a83270afa3f3a3da594afec0b6866d1c
jhonpulido1990 <3825holbertonstudents.com>
2024-03-07 11:05:38 -0500

    debounce peticion cuando el usuario deja de escribir

--- angular/04-countryApp/src/app/countries/services/countries.service.ts
+++ angular/04-countryApp/src/app/countries/services/countries.service.ts
@@ -3,44 +3,39 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, catchError, delay, map, of } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class CountriesService {
+  private apiUrl: string = 'https://restcountries.com/v3.1';
 
-  private apiUrl: string ='https://restcountries.com/v3.1';
+  constructor(private http: HttpClient) {}
 
-  constructor(
-    private http: HttpClient) { }
-
-  private getCountriesResquest( url: string ): Observable<Country[]>{
-    return this.http.get<Country[]>(url)
-    .pipe(
-      catchError( error => of([]) ),
-      delay( 2000 ),
-    )
-  }
-
-  searchCountryByAlphaCode( code: string ): Observable<Country | null > {
-    const url = `${this.apiUrl}/alpha/${code}`
-    return this.http.get<Country[]>( url )
-    .pipe(
-      map( countries => countries.length > 0 ? countries[0]: null ),
-      catchError( () => of(null) )
+  private getCountriesResquest(url: string): Observable<Country[]> {
+    return this.http.get<Country[]>(url).pipe(
+      catchError((error) => of([])),
+     /*  delay(2000) */
     );
   }
 
-  searchCapital( term: string ): Observable<Country[]> {
-    const url = `${this.apiUrl}/capital/${term}`
+  searchCountryByAlphaCode(code: string): Observable<Country | null> {
+    const url = `${this.apiUrl}/alpha/${code}`;
+    return this.http.get<Country[]>(url).pipe(
+      map((countries) => (countries.length > 0 ? countries[0] : null)),
+      catchError(() => of(null))
+    );
+  }
+
+  searchCapital(term: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/capital/${term}`;
     return this.getCountriesResquest(url);
   }
 
-  searchCountry( term: string ): Observable<Country[]> {
-    const url = `${this.apiUrl}/name/${term}`
-    return this.getCountriesResquest(url)
+  searchCountry(term: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/name/${term}`;
+    return this.getCountriesResquest(url);
   }
 
-  searchRegion( term: string ): Observable<Country[]> {
-    const url = `${this.apiUrl}/region/${term}`
-    return this.getCountriesResquest(url)
+  searchRegion(term: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/region/${term}`;
+    return this.getCountriesResquest(url);
   }
-
 }
--- angular/04-countryApp/src/app/shared/components/searchBox/searchBox.component.ts
+++ angular/04-countryApp/src/app/shared/components/searchBox/searchBox.component.ts
@@ -1,11 +1,14 @@
-import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-searchBox',
   templateUrl: './searchBox.component.html',
   styleUrls: ['./searchBox.component.css']
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit {
+
+  private debocer = new Subject<string>();
 
   @Input()
   public placeholder: string = '';
@@ -13,13 +16,30 @@ export class SearchBoxComponent {
   @Output()
   public onValue: EventEmitter<string> = new EventEmitter();
 
+  @Output()
+  public onDebounce: EventEmitter<string> = new EventEmitter();
+
   @ViewChild('txtSearchInput')
   public TagInput!: ElementRef<HTMLInputElement>;
+
+  ngOnInit(): void {
+    this.debocer
+    .pipe(
+      debounceTime(1000)
+    )
+    .subscribe( value => {
+      this.onDebounce.emit(value);
+    } )
+  }
 
   searchInput() {
     const newTag = this.TagInput.nativeElement.value;
     this.onValue.emit(newTag);
     this.TagInput.nativeElement.value = '';
+  }
+
+  onKeyPress( serachTerm: string ){
+    this.debocer.next( serachTerm )
   }
 
 }
