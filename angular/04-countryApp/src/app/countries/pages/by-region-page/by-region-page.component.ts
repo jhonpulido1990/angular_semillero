e99f9acc838350f533be04f2ee7efe435f956d22
jhonpulido1990 <3825holbertonstudents.com>
2024-03-07 11:59:05 -0500

    busqueda poe region Optimizado

--- angular/04-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ angular/04-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country.interface';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania'
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -9,10 +11,13 @@ import { CountriesService } from '../../services/countries.service';
 })
 export class ByRegionPageComponent {
   public countries: Country[] = [];
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectRegion?: Region
 
   constructor(private countryService: CountriesService) {}
 
-  searchByRegion(term: string): void {
+  searchByRegion(term: Region): void {
+    this.selectRegion = term;
     this.countryService.searchRegion(term).subscribe((countries) => {
       this.countries = countries;
     });
