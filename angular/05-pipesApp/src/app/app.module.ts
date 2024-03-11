6a1b10178223376e43ad2372686a27bfab008c26
jhonpulido1990 <3825holbertonstudents.com>
2024-03-10 19:23:56 -0500

    cambiar idioma

--- angular/05-pipesApp/src/app/app.module.ts
+++ angular/05-pipesApp/src/app/app.module.ts
@@ -1,23 +1,32 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 
+// ? configuracion del locale de la app
+import localeEsHN from '@angular/common/locales/es-HN';
+
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEsHN);
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es-HN',
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
