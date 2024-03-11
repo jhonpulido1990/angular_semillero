bf4225dfa379b50044d0c18743522c87143def72
jhonpulido1990 <3825holbertonstudents.com>
2024-03-10 22:08:51 -0500

    i18nPlural

--- angular/05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ angular/05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,3 +1,4 @@
+import { I18nPluralPipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -18,5 +19,17 @@ export class UncommonPageComponent {
   changeClient() {
     this.name = 'Melissa',
     this.gender = 'female'
+  }
+
+  // i18Plural
+  public clients: string[] = ['Marias', 'Fernando', 'Pedro', 'Hernando', 'Eduardo', 'Melisa', 'Natalia'];
+  public clientsMap = {
+    '=0': 'no tenemos clientes esperando',
+    '=1': 'tenemos un cliente esperando',
+    'other': 'tenemos # de clientes esperando'
+  }
+
+  deleteClient() {
+    this.clients.shift();
   }
 }
