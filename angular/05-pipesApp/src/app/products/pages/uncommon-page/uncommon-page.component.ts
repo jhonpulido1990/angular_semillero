dc73b43497c7cef6559d57e7cbd4f99cd1fb2ac0
jhonpulido1990 <3825holbertonstudents.com>
2024-03-11 11:56:56 -0500

    asyncPipe

--- angular/05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ angular/05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,5 +1,6 @@
 import { I18nPluralPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { Observable, interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -32,4 +33,24 @@ export class UncommonPageComponent {
   deleteClient() {
     this.clients.shift();
   }
+
+  //KeyValue pipe
+  public person = {
+    name: 'Fernando',
+    age: 36,
+    address: 'Ottawa, Canada'
+  }
+
+  // Async Pipe
+  public myObservableTimer: Observable<number>  = interval(2000).pipe(
+    tap( value => console.log('tap: ', value) )
+  )
+
+  public promiseValue: Promise<string> = new Promise( ( resolve, reject ) => {
+    setTimeout( () => {
+      resolve( 'Tenemos data en la promesa' );
+      console.log('tenemos data en la promesa');
+      this.person.name = 'Otro Nombre'
+    }, 3500 )
+  } )
 }
