55bbb05652b2e8dd4eecf65953abb5859069c900
jhonpulido1990 <3825holbertonstudents.com>
2024-03-11 16:48:43 -0500

    argumentos dinamicos a pipes personalizados

--- angular/05-pipesApp/src/app/products/pages/order/order.component.ts
+++ angular/05-pipesApp/src/app/products/pages/order/order.component.ts
@@ -1,24 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrl: './order.component.css',
 })
-export class OrderComponent implements OnInit {
-  items: MenuItem[] | undefined;
+export class OrderComponent {
 
-  ngOnInit(): void {
-    this.items = [
-      {
-        label: 'Update',
-        icon: 'pi pi-refresh',
-      },
-      {
-        label: 'Delete',
-        icon: 'pi pi-times',
-      },
-    ];
+  public isUpperCase: boolean = false;
+
+  toogleUpperCase(): void {
+    this.isUpperCase = !this.isUpperCase
   }
 }
