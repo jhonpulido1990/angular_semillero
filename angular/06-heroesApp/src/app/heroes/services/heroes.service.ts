326da382d487ef63bd074610ca6e1cf406151e80
jhonpulido1990 <3825holbertonstudents.com>
2024-03-18 15:03:29 -0500

    CRUD Endpoints

--- angular/06-heroesApp/src/app/heroes/services/heroes.service.ts
+++ angular/06-heroesApp/src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../../environments/environments';
 
@@ -24,5 +24,21 @@ export class HeroService {
 
   getSuggestions( query: string ): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${ this.baseUrl }/heroes?q=${ query }&_limit=6`);
+  }
+
+  addHero( hero: Hero ): Observable<Hero> {
+    return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero)
+  }
+
+  updateHero ( hero: Hero ): Observable<Hero> {
+    return this.http.patch<Hero>(`${ this.baseUrl }/heroes/${ hero.id }`, hero)
+  }
+
+  deleteHero( id: string ): Observable<boolean> {
+    return this.http.delete(`${ this.baseUrl }/heroes/${id}`)
+    .pipe(
+      catchError( err => of(false)),
+      map( res => true )
+    )
   }
 }
