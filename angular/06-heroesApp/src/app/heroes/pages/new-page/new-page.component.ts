648833b63482eff0b3f0fb43b4136729df94caff
jhonpulido1990 <3825holbertonstudents.com>
2024-03-19 17:05:05 -0500

    establecer el valor del formulario

--- angular/06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
+++ angular/06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,22 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Hero, Publisher } from '../../interfaces/hero.interface';
 import { HeroService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
   styles: ``,
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit {
   public heroForm = new FormGroup({
-    id:               new FormControl<string>(''),
-    superhero:        new FormControl<string>('', { nonNullable: true }),
-    publisher:        new FormControl<Publisher>( Publisher.DCComics ),
-    alter_ego:        new FormControl(''),
+    id: new FormControl<string>(''),
+    superhero: new FormControl<string>('', { nonNullable: true }),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl(''),
     first_appearance: new FormControl(''),
-    characters:       new FormControl(''),
-    alt_img:          new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
   });
 
   public publishers = [
@@ -24,7 +26,24 @@ export class NewPageComponent {
     { id: 'Marvel Comics', desc: 'Marvel - Comics' },
   ];
 
-  constructor( private heroService: HeroService ) {}
+  constructor(
+    private heroService: HeroService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    if ( !this.router.url.includes('edit') ) return;
+
+    this.activatedRoute.params
+    .pipe(
+      switchMap( ({ id }) => this.heroService.getHeroById( id ) ),
+    ).subscribe( hero => {
+      if ( !hero ) return this.router.navigateByUrl('/');
+      this.heroForm.reset( hero )
+      return;
+    } )
+  }
 
   get currentHero() {
     const hero = this.heroForm.value as Hero;
@@ -34,17 +53,15 @@ export class NewPageComponent {
 
   onSumit() {
     if (this.heroForm.invalid) return;
-    if ( this.currentHero.id ) {
-      this.heroService.updateHero( this.currentHero )
-      .subscribe( hero => {
+    if (this.currentHero.id) {
+      this.heroService.updateHero(this.currentHero).subscribe((hero) => {
         // todo: mostrar snackbar
-      } );
-      return
+      });
+      return;
     }
 
-    this.heroService.addHero( this.currentHero )
-    .subscribe( hero => {
+    this.heroService.addHero(this.currentHero).subscribe((hero) => {
       // todo: mostar snackbar y navegar a /heroes/edit hero.id
-    })
+    });
   }
 }
