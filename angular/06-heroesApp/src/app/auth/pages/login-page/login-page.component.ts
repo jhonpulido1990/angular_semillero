17fb5a856ec24a7f8eda968a9b77bf5f52aa8b9f
jhonpulido1990 <3825holbertonstudents.com>
2024-03-20 23:42:08 -0500

    Autenticar y mostrar usuario activo

--- angular/06-heroesApp/src/app/auth/pages/login-page/login-page.component.ts
+++ angular/06-heroesApp/src/app/auth/pages/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class LoginPageComponent {
 
+  constructor( private authservice: AuthService, private router: Router ) {}
+
+  onLogin(): void {
+    this.authservice.login('jhonpulido@hotmail.com', '123456')
+    .subscribe( result => {
+      this.router.navigate(['/'])
+    } )
+  }
 }
