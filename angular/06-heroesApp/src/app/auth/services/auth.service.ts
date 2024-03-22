98999c07a0a659dc6a12873021e62a6fbe22e93f
jhonpulido1990 <3825holbertonstudents.com>
2024-03-22 09:51:41 -0500

    mantener la seccion de usuario

--- angular/06-heroesApp/src/app/auth/services/auth.service.ts
+++ angular/06-heroesApp/src/app/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environments } from '../../../environments/environments';
 import { User } from '../interfaces/user.interface';
-import { Observable, tap } from 'rxjs';
+import { Observable, catchError, map, of, tap } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
@@ -22,6 +22,18 @@ export class AuthService {
       tap((user) => (this.user = user)),
       tap((user) => localStorage.setItem('token', user.id.toString()))
     );
+  }
+
+  checkAutentication(): Observable<boolean> | false {
+    if (!localStorage.getItem('token')) return false;
+    const token = localStorage.getItem('token');
+
+    return this.http.get<User>(`${ this.baseUrl }/users/1`)
+    .pipe(
+      tap( user => this.user = user ),
+      map( user => !!user ),
+      catchError( err => of(false) )
+    )
   }
 
   logout() {
