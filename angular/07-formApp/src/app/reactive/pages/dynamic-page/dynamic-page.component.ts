64b626a9882b8e734ecb36a62449449effc1c9e5
jhonpulido1990 <3825holbertonstudents.com>
2024-03-26 16:24:05 -0500

    eliminar elementos de un array

--- angular/07-formApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ angular/07-formApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -49,6 +49,10 @@ export class DynamicPageComponent {
     return null;
   }
 
+  onDeleteFavorite( index: number ): void {
+    this.favoriteGames.removeAt(index)
+  }
+
   onSubmit():void {
 
     if ( this.myForm.invalid ) {
