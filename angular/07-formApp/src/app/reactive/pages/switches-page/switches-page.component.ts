c33f5ba0f4172f8a082ab8f187b10ec74b84dfba
jhonpulido1990 <3825holbertonstudents.com>
2024-03-27 00:07:26 -0500

    tarea

--- angular/07-formApp/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ angular/07-formApp/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches-page',
@@ -7,4 +8,42 @@ import { Component } from '@angular/core';
 })
 export class SwitchesPageComponent {
 
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue],
+  })
+
+  constructor( private fb: FormBuilder ) {}
+
+  isValidField( field: string ): boolean | null {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched
+  }
+
+  getFieldError( field: string ): string | null {
+    if ( !this.myForm.controls[field] ) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for(const key of Object.keys(errors)) {
+      switch( key ) {
+        case 'required':
+        return 'Este campo es requerido';
+
+        case 'requiredTrue':
+        return 'Debe de aceptar las condiciones de uso'
+      }
+    }
+    return null;
+  }
+
+  onSave() {
+    if ( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log( this.myForm.value );
+    this.myForm.reset({ gender: 'M', wantNotifications: true, termsAndConditions: false })
+  }
 }
