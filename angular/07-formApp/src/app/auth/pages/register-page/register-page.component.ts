bac1555ee3f5998a6486bcd7a0e9f2b1a6c01658
jhon-puli <3825@holbertonstudents.com>
2024-03-28 17:12:51 -0500

    funciones validadoras

--- angular/07-formApp/src/app/auth/pages/register-page/register-page.component.ts
+++ angular/07-formApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cantStrider } from '../../../shared/validators/validators';
 
 @Component({
   selector: 'app-register-page',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class RegisterPageComponent {
 
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required ]],
+    email: ['', [ Validators.required ]],
+    username: ['', [ Validators.required, cantStrider ]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]],
+    password2: ['', [ Validators.required ]]
+  })
+
+  constructor( private fb: FormBuilder ) {}
+
+  isValidField( field: string ){
+    //todo: obtener validacion de un servicio
+  }
+
+  onSave() {
+    this.myForm.markAllAsTouched();
+  }
 }
