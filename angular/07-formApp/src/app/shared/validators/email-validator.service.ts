2231b38ec0c9e7941f6b0c06d2ddf9039c28f74d
jhon-puli <3825@holbertonstudents.com>
2024-03-29 16:37:36 -0500

    validando el correo

--- angular/07-formApp/src/app/shared/validators/email-validator.service.ts
+++ angular/07-formApp/src/app/shared/validators/email-validator.service.ts
@@ -1,14 +1,29 @@
 import { Injectable } from '@angular/core';
-import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
+import {
+  AbstractControl,
+  AsyncValidator,
+  ValidationErrors,
+} from '@angular/forms';
 import { Observable, of } from 'rxjs';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class EmailValidatorService implements AsyncValidator {
-  validate(control: AbstractControl<any, any>): | Observable<ValidationErrors | null> {
+  validate(
+    control: AbstractControl<any, any>
+  ): Observable<ValidationErrors | null> {
     const email = control.value;
-    return of({
-      emailTaken: true
-    })
-  }
 
+    const httpCallObservable = new Observable<ValidationErrors | null>(
+      (subscriber) => {
+        if (email === 'jjpulido8@misena.edu.co') {
+          subscriber.next({ emailTaken: true });
+          subscriber.complete();
+          return;
+        }
+        subscriber.next(null);
+        subscriber.complete();
+      }
+    );
+    return httpCallObservable;
+  }
 }
