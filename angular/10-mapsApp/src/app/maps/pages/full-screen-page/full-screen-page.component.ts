195ff4f47851c501c9b32e7ab75b60ed71c74707
jhon-puli <3825@holbertonstudents.com>
2024-04-01 22:03:41 -0500

    mostrar un mapa en pantalla

--- angular/10-mapsApp/src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ angular/10-mapsApp/src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
+(mapboxgl as any).accessToken = 'pk.eyJ1IjoiamhvbnB1bGkiLCJhIjoiY2x1aGYwYTlhMnB1MDJtbjQ0eThxaGgwbyJ9.YLDa0xgrSupMdgnxvX8NRQ';
 
 @Component({
   templateUrl: './full-screen-page.component.html',
-  styleUrl: './full-screen-page.component.css'
+  styleUrl: './full-screen-page.component.css',
 })
-export class FullScreenPageComponent {
-
+export class FullScreenPageComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
 }
