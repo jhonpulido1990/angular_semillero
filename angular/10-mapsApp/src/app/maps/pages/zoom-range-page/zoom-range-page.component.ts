832544a28ee40c3a07c5cadf955a6c2afeaa6a24
jhon-puli <3825@holbertonstudents.com>
2024-04-02 22:56:44 -0500

    controlar el nivel del zoom

--- angular/10-mapsApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ angular/10-mapsApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -8,17 +8,47 @@ import { Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 })
 export class ZoomRangePageComponent implements AfterViewInit {
 
+  public zoom: number = 10;
+  public map?: Map;
+
   @ViewChild('map') divMap?: ElementRef;
 
   ngAfterViewInit(): void {
     if ( !this.divMap ) throw 'Elemento no encontrado'
 
-    const map = new Map({
+    this.map = new Map({
       container: this.divMap.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v12',
       center: [-74.5, 40],
-      zoom: 9,
+      zoom: this.zoom,
     })
+    this.mapListener()
+  }
+
+  mapListener() {
+    if ( !this.map ) throw 'Mapa no inicializado';
+
+    this.map.on('zoom', (ev) => {
+      this.zoom = this.map!.getZoom();
+    })
+
+    this.map.on('zoomend', (ev) => {
+      if ( this.map!.getZoom() < 18 ) return;
+      this.map!.zoomTo(18);
+    })
+  }
+
+  zoomIn() {
+    this.map?.zoomIn();
+  }
+
+  zoomOut() {
+    this.map?.zoomOut();
+  }
+
+  zoomChanged( value: string ) {
+    this.zoom = Number( value )
+    this.map?.zoomTo(this.zoom)
   }
 
 }
