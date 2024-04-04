944f2708ad0e04299aa0c9c64ccc35a0e885167e
jhon-puli <3825@holbertonstudents.com>
2024-04-04 17:31:10 -0500

    componente tradicional a un Standalone

--- angular/10-mapsApp/src/app/alone/pages/alone-page/alone-page.component.ts
+++ angular/10-mapsApp/src/app/alone/pages/alone-page/alone-page.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { CounterAloneComponent } from '../../components/counter-alone/counter-alone.component';
+import { SideMenuComponent } from '../../components/side-menu/side-menu.component';
 
 @Component({
   selector: 'app-alone-page',
   standalone: true,
-  imports: [ CommonModule, CounterAloneComponent ],
+  imports: [ CommonModule, CounterAloneComponent, SideMenuComponent ],
   templateUrl: './alone-page.component.html',
   styleUrl: './alone-page.component.css'
 })
--- angular/10-mapsApp/src/app/maps/maps.module.ts
+++ angular/10-mapsApp/src/app/maps/maps.module.ts
@@ -10,9 +10,9 @@ import { MapsRoutingModule } from './maps-routing.module';
 import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
 import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
-import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
 import { CounterAloneComponent } from '../alone/components/counter-alone/counter-alone.component';
+import { SideMenuComponent } from '../alone/components/side-menu/side-menu.component';
 
 
 @NgModule({
@@ -22,13 +22,13 @@ import { CounterAloneComponent } from '../alone/components/counter-alone/counter
     MarkersPageComponent,
     MiniMapComponent,
     PropertiesPageComponent,
-    SideMenuComponent,
     ZoomRangePageComponent
   ],
   imports: [
     CommonModule,
     MapsRoutingModule,
-    CounterAloneComponent
+    CounterAloneComponent,
+    SideMenuComponent,
   ]
 })
 export class MapsModule { }
