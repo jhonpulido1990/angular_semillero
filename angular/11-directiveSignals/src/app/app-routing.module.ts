f68c0ded7dc2db2893d98a8f1d8f9ae7643b5f76
jhon-puli <3825@holbertonstudents.com>
2024-04-07 17:12:57 -0500

    continuacion del projecto

--- angular/11-directiveSignals/src/app/app-routing.module.ts
+++ angular/11-directiveSignals/src/app/app-routing.module.ts
@@ -8,9 +8,14 @@ const routes: Routes = [
       import('./products/products.module').then((m) => m.ProductsModule),
   },
   {
+    path: 'signals',
+    loadChildren: () =>
+      import('./signals/signals.module').then((m) => m.SignalsModule),
+  },
+  {
     path: '**',
     redirectTo: 'products',
-  }
+  },
 ];
 
 @NgModule({
