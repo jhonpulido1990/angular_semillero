f90fe3280775ff51ecbd80d3677ccb5632ee73c1
jhon-puli <3825@holbertonstudents.com>
2024-04-08 10:41:11 -0500

    señales informacion del usuario

--- angular/11-directiveSignals/src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ angular/11-directiveSignals/src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { UsersserviceService } from '../../services/usersservice.service';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './user-info-page.component.html',
-  styleUrl: './user-info-page.component.css'
+  styleUrl: './user-info-page.component.css',
 })
-export class UserInfoPageComponent {
+export class UserInfoPageComponent implements OnInit {
 
+  private userService = inject(UsersserviceService);
+  public userId = signal(1);
+
+  currentUser = signal<User | undefined>(undefined);
+  public userWasFound = signal(true);
+
+  ngOnInit(): void {
+    this.loadUser(this.userId())
+  }
+
+  loadUser(id: number) {
+    if (id <= 0)return;
+    this.userId.set(id);
+    this.currentUser.set(undefined);
+    this.userService.getUsersByID(id)
+    .subscribe((user: User | undefined) => {
+      this.currentUser.set(user);
+    });
+  }
 }
