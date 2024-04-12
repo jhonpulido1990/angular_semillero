ae3ca8b0c61c7d792d781f8beb56f4f50e6f7a08
jhon-puli <3825@holbertonstudents.com>
2024-04-11 22:29:50 -0500

    formulario de inicio

--- angular/13-authApp/src/app/auth/auth.module.ts
+++ angular/13-authApp/src/app/auth/auth.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AuthRoutingModule } from './auth-routing.module';
 import { AuthLayoutComponent } from './layouts/authLayout/authLayout.component';
+import { AuthRoutingModule } from './auth-routing.module';
+import { LoginPageComponent } from './pages/loginPage/loginPage.component';
+import { RegisterPageComponent } from './pages/registerPage/registerPage.component';
 
 
 @NgModule({
-  declarations: [AuthLayoutComponent],
+  declarations: [
+    AuthLayoutComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+  ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule,
   ]
 })
 export class AuthModule { }
--- angular/13-authApp/src/app/auth/pages/loginPage/loginPage.component.ts
+++ angular/13-authApp/src/app/auth/pages/loginPage/loginPage.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-loginPage',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
+  private fb = inject(FormBuilder);
+
+  public myForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  })
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  login() {
+    console.log(this.myForm.value);
   }
 
 }
