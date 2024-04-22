2b1d77bae52de3d1686a87c57ed7b7b658132ade
jhon-puli <3825@holbertonstudents.com>
2024-04-22 17:25:18 -0500

    configuracion de rutas

--- angular/14-my-dashboard/src/app/app.routes.ts
+++ angular/14-my-dashboard/src/app/app.routes.ts
@@ -1,3 +1,59 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  /* {
+    path: 'dashboard',
+    loadComponent: () => import('./dashboard/dashboard.component').then( c => c.DashboardComponent )
+  } */
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./dashboard/dashboard.component'),
+    children: [
+      {
+        path: 'change-detection',
+        title: 'Change-detection',
+        loadComponent: () => import('./dashboard/pages/change-detection/change-detection.component'),
+      },
+      {
+        path: 'control-flow',
+        title: 'Control-flow',
+        loadComponent: () => import('./dashboard/pages/control-flow/control-flow.component'),
+      },
+      {
+        path: 'defer-option',
+        title: 'Defer-option',
+        loadComponent: () => import('./dashboard/pages/defer-options/defer-options.component'),
+      },
+      {
+        path: 'defer-view',
+        title: 'Defer-view',
+        loadComponent: () => import('./dashboard/pages/defer-views/defer-views.component'),
+      },
+      {
+        path: 'user/:id',
+        title: 'User-view',
+        loadComponent: () => import('./dashboard/pages/user/user.component'),
+      },
+      {
+        path: 'user-list',
+        title: 'User-list',
+        loadComponent: () => import('./dashboard/pages/users/users.component'),
+      },
+      {
+        path: 'view-transition',
+        title: 'View-transition',
+        loadComponent: () => import('./dashboard/pages/view-transition/view-transition.component'),
+      },
+      {
+        path: '',
+        redirectTo: 'control-flow',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  }
+];
