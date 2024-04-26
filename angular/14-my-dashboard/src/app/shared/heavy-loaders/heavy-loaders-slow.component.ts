477d413c652628c4c96aded8cad42ffa2ee4d6dc
jhon-puli <3825@holbertonstudents.com>
2024-04-26 16:52:43 -0500

    defer block

--- angular/14-my-dashboard/src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
+++ angular/14-my-dashboard/src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-slow',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
-  <h1>hola mundo cruel</h1>
-  `
+    <section [ngClass]="['w-full h-[600px]', cssClass]">
+      heavy loader slow
+    </section>
+  `,
 })
 export class HeavyLoadersSlowComponent {
-
+  @Input({ required: true }) cssClass!: string;
+  constructor() {
+    const start = Date.now();
+    while (Date.now() - start < 3000) {}
+    console.log('Cargado');
+  }
 }
