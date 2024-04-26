c4acecee2a5e56d4c96aed78d1b6f1316d3b2d74
jhon-puli <3825@holbertonstudents.com>
2024-04-26 11:13:24 -0500

    control flow

--- angular/14-my-dashboard/src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ angular/14-my-dashboard/src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './control-flow.component.html',
   styles: ``
 })
 export default class ControlFlowComponent {
 
+  public showContent = signal(false);
+
+  public toggleContent() {
+    this.showContent.update(value => !value);
+  }
 }
