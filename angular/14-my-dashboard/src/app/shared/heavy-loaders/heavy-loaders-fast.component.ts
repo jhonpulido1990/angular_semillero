6bb06f97223a9db9c0a8e8568217a8f14eb7c347
jhon-puli <3825@holbertonstudents.com>
2024-04-26 22:39:11 -0500

    defer triggers interacciones

--- angular/14-my-dashboard/src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
+++ angular/14-my-dashboard/src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-fast',
   standalone: true,
-  imports: [],
-  template: ` <h1>hola mundo cruel fast</h1> `,
+  imports: [CommonModule],
+  template: `
+  <section [ngClass]="['w-full', cssClass]">
+    <ng-content />
+
+  </section>
+  `,
 })
-export class HeavyLoadersFastComponent {}
+export class HeavyLoadersFastComponent {
+  @Input({required: true }) cssClass!: string;
+}
